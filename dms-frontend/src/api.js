6e1022ac4658341559b41807d32107b9d18d3375
unknown <messoudiahmed07@gmail.com>
2025-07-28 22:38:03 +0100

    DragDropUplaod Update & Log Listing Update

--- dms-frontend/src/api.js
+++ dms-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Create axios instance with base configuration
-const API_BASE_URL = 'http://192.168.1.28:5000';
+const API_BASE_URL = 'http://localhost:5000';
 
 const API = axios.create({
   baseURL: API_BASE_URL,
