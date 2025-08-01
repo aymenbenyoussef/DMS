0d9821e8cbb5d860fa93ff8509c0fb713484e897
unknown <messoudiahmed07@gmail.com>
2025-08-01 16:49:29 +0100

    Put on Production Mode & Resize the site

--- dms-frontend/src/index.js
+++ dms-frontend/src/index.js
@@ -4,5 +4,20 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
+
+// Désactiver tous les logs natifs si REACT_APP_PRODUCTION_LOGS est à 'true'
+if (process.env.REACT_APP_PRODUCTION_LOGS === 'true') {
+  // eslint-disable-next-line no-empty-function
+  console.log = () => {};
+  // eslint-disable-next-line no-empty-function
+  console.error = () => {};
+  // eslint-disable-next-line no-empty-function
+  console.warn = () => {};
+  // eslint-disable-next-line no-empty-function
+  console.info = () => {};
+  // eslint-disable-next-line no-empty-function
+  console.debug = () => {};
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
